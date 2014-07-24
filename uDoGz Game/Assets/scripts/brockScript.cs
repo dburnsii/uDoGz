c0e4506a2617a5925b93f785a5681e84fbb3e36d
dburnsii <dburnsii@email.arizona.edu>
2014-07-23 19:46:41 -0700

    Initial Commit
    Includes placeholder sprites and animations for walking. TODO: Collsion
    handling.

---
+++ uDoGz Game/Assets/scripts/brockScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class brockScript : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public float FPS;
+	private SpriteRenderer spriteRenderer;
+	private Vector3 moveDirection;
+	private Vector3 currentPosition;
+	public float speed;
+	private int dir;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		spriteRenderer = renderer as SpriteRenderer;
+		dir = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		currentPosition = transform.position;
+		if (dir == 0) 
+		{
+			walkUp ();
+		} 
+		else 
+		{
+			walkDown ();
+		}
+		//int index = (int)(Time.timeSinceLevelLoad * FPS);
+		//index = index % sprites.Length;
+		//spriteRenderer.sprite = sprites [index];
+	}
+
+	void walkUp()
+	{
+		if (transform.position.y > 3) {
+			dir = 1;
+			return;
+				}
+		int index = (int)(Time.timeSinceLevelLoad * FPS);
+		index = index % (sprites.Length - 4);
+		moveDirection.Set(0, 1, 0);
+		//transform.position.Set((float) 2, (float) (currentPosition.y + 0.1), (float) 0.0);
+		moveDirection.Normalize ();
+		Vector3 target = moveDirection*speed + currentPosition;
+		transform.position = Vector3.Lerp (currentPosition, target, Time.deltaTime);
+		spriteRenderer.sprite = sprites [index+4];
+	}
+
+	void walkDown()
+	{
+		if (transform.position.y < -3) {
+			dir = 0;
+			return;
+				}
+		int index = (int)(Time.timeSinceLevelLoad * FPS);
+		index = index % (sprites.Length - 4);
+		moveDirection.Set(0, -1, 0);
+		moveDirection.Normalize ();
+		Vector3 target = moveDirection*speed + currentPosition;
+		transform.position = Vector3.Lerp (currentPosition, target, Time.deltaTime);
+		spriteRenderer.sprite = sprites [index];
+	}
+
+	void OnCollisionEnter(Collider coll)
+	{
+
+		}
+}
---
+++ uDoGz Game/Assets/scripts/playerMovementScript.cs
@@ -0,0 +1,222 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovementScript : MonoBehaviour  {
+
+	private Vector3 dest;
+	private Vector3 start;
+	private Vector3 target;
+	public float speed;
+	private SpriteRenderer spriteRenderer;
+	public Sprite[] sprites;
+	private char dir;
+	public float moveTime;
+	private bool done;
+	private KeyCode last;
+	public float FPS;
+	public float idleTime;
+	private bool ubool;
+	private bool dbool;
+	private bool lbool;
+	private bool rbool;
+	private bool coll;
+
+	// Use this for initialization
+	void Start () {
+		coll = false;
+		spriteRenderer = renderer as SpriteRenderer;
+		start = transform.position;
+		done = true;
+		moveTime = 0;
+		speed = speed * 0.075f;
+		ubool = false;
+		dbool = false;
+		lbool = false;
+		rbool = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (done || !coll) {
+						if (Input.GetKey (last)) {
+								if (last.ToString () == "UpArrow") {
+										moveUp ();
+										return;
+								}
+								if (last.ToString () == "DownArrow") {
+										moveDown ();
+										return;
+								}
+								if (last.ToString () == "LeftArrow") {
+										moveLeft ();
+										return;
+								}
+								if (last.ToString () == "RightArrow") {
+										moveRight ();
+										return;
+								}
+						} else if (Input.GetKey (KeyCode.DownArrow)) {
+								moveDown ();
+								last = KeyCode.DownArrow;
+								return;
+						} else if (Input.GetKey (KeyCode.LeftArrow)) {
+								moveLeft ();
+								last = KeyCode.LeftArrow;
+								return;
+						} else if (Input.GetKey (KeyCode.RightArrow)) {
+								moveRight ();
+								last = KeyCode.RightArrow;
+								return;
+						} else if (Input.GetKey (KeyCode.UpArrow)) {
+								moveUp ();
+								last = KeyCode.UpArrow;
+								return;
+						} else {
+								idleTime = Time.time;
+								if (last.ToString () == "UpArrow") {
+										spriteRenderer.sprite = sprites [0];
+										return;
+								}
+								if (last.ToString () == "DownArrow") {
+										spriteRenderer.sprite = sprites [4];
+										return;
+								}
+								if (last.ToString () == "LeftArrow") {
+										spriteRenderer.sprite = sprites [8];
+										return;
+								}
+								if (last.ToString () == "RightArrow") {
+										spriteRenderer.sprite = sprites [12];
+										return;
+
+								}
+						} 
+				}
+		else 
+		{
+			if (dir == 'u') 
+			{
+				moveUp ();
+			}
+			if (dir == 'd') 
+			{
+				moveDown ();
+			}
+			if (dir == 'l')
+			{
+				moveLeft ();
+			}
+			if (dir == 'r') 
+			{
+				moveRight ();
+			}
+		}
+	}
+
+	void moveUp()
+	{
+		dest.Set (0, 1, 0);
+		target = start + dest;
+		if (moveTime >= 1) 
+		{
+			moveTime = 0;
+			done = true;
+			start = transform.position;
+		}
+		else 
+		{
+			int index = (int)(Time.timeSinceLevelLoad * FPS);
+			index = index % (sprites.Length - 12);
+			spriteRenderer.sprite = sprites[index];
+			moveTime += speed;
+			transform.position = Vector3.Lerp (start, target, moveTime);
+			dir = 'u';
+			done = false;
+		}
+	}
+
+	void moveDown()
+	{
+		dest.Set (0, -1, 0);
+		target = start + dest;
+		if (moveTime >= 1) 
+		{
+			moveTime = 0;
+			done = true;
+			start = transform.position;
+		} 
+		else 
+		{
+			int index = (int)(Time.timeSinceLevelLoad * FPS);
+			index = index % (sprites.Length - 12);
+			spriteRenderer.sprite = sprites[index + 4];
+			moveTime += speed;
+			transform.position = Vector3.Lerp (start, target, moveTime);
+			dir = 'd';
+			done = false;
+		}
+	}
+
+	void moveLeft()
+	{
+		dest.Set (-1, 0, 0);
+		target = start + dest;
+		if (moveTime >= 1) 
+		{
+			moveTime = 0;
+			done = true;
+			start = transform.position;
+		} 
+		else 
+		{
+			int index = (int)(Time.timeSinceLevelLoad * FPS);
+			index = index % (sprites.Length - 12);
+			spriteRenderer.sprite = sprites[index + 8];
+			moveTime += speed;
+			transform.position = Vector3.Lerp (start, target, moveTime);
+			dir = 'l';
+			done = false;
+		}
+	}
+
+	void moveRight()
+	{
+		dest.Set (1, 0, 0);
+		target = start + dest;
+		if (moveTime >= 1) 
+		{
+			moveTime = 0;
+			done = true;
+			start = transform.position;
+		} 
+		else 
+		{
+			int index = (int)(Time.timeSinceLevelLoad * FPS);
+			index = index % (sprites.Length - 12);
+			spriteRenderer.sprite = sprites[index + 12];
+			moveTime += speed;
+			transform.position = Vector3.Lerp (start, target, moveTime);
+			dir = 'r';
+			done = false;
+		}
+	}
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Building") 
+		{
+			Debug.Log("Hit a building");
+			speed = 0;
+		}
+		Debug.Log (coll.gameObject.tag + " - tag");
+	}
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Building") 
+		{
+			Debug.Log("Hit a building");
+			speed = 0.075f;
+		}
+		Debug.Log (coll.gameObject.tag + " - tag");
+	}
+}
